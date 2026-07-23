66c9b8dd43d46230e3fd3d3c9c0824452f1e3b83
Saksham Sharma <sakshams1aug@gmail.com>
2026-07-24 00:30:11 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (13.43%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -15,26 +15,18 @@ public class Solution {
         ListNode fast = head;
         
         while (fast != null && fast.next != null) {
-            slow = slow.next;         // move slow by 1
-            fast = fast.next.next;    // move fast by 2
-            
-            // 1. Check if they meet (Cycle exists)
+            slow = slow.next;         
+            fast = fast.next.next;    
+        
             if (slow == fast) {
                 ListNode p = head;
-                
-                // 2. Find the start of the cycle
-                // Move both pointers ONE step at a time until they meet
                 while (p != slow) {
                     p = p.next;
                     slow = slow.next;
                 }
-                
-                // The meeting point is the start of the cycle
                 return p;
             }
         }
-        
-        // If the loop finishes, fast reached the end, so there is no cycle
         return null;
     }
 }
